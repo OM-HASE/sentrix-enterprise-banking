955b958ce00c22ba6e78baf6679b9852904b2256
OM HASE <138221910+OM-HASE@users.noreply.github.com>
2026-05-22 22:15:02 +0530

    SentriX repair for src/database.cpp

--- src/database.cpp
+++ src/database.cpp
@@ -6,50 +6,36 @@
 using namespace std;
 
 void connectDatabase() {
-
-    ifstream file(
-        "data/customers.txt"
-    );
-
-    // ERROR 8
-    // File open failure
+    ifstream file("data/customers.txt");
+    if (!file.is_open()) {
+        cerr << "Error: Unable to open file" << endl;
+        return;
+    }
 
     string line;
-
     getline(file, line);
 
-    cout
-        << line
-        << endl;
+    cout << line << endl;
 }
 
 void loadCustomers() {
-
-    // ERROR 9
-    // Memory leak
-
-    int* data = new int[100000];
+    int* data = new (std::nothrow) int[100000];
+    if (!data) {
+        cerr << "Error: Memory allocation failed" << endl;
+        return;
+    }
 
     data[0] = 100;
 
-    cout
-        << data[0]
-        << endl;
+    cout << data[0] << endl;
+
+    delete[] data;
 }
 
 void unsafeFileParser() {
-
-    // ERROR 10
-    // Buffer overflow
-
     char buffer[10];
+    strncpy(buffer, "THIS_IS_A_LONG_CUSTOMER_RECORD", sizeof(buffer) - 1);
+    buffer[sizeof(buffer) - 1] = '\0';
 
-    strcpy(
-        buffer,
-        "THIS_IS_A_LONG_CUSTOMER_RECORD"
-    );
-
-    cout
-        << buffer
-        << endl;
+    cout << buffer << endl;
 }
